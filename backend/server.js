57d29ae150fd4ddf44296df26a131b2b94aac1fc
Vxn1-coder <vxn1verse@gmail.com>
2026-04-22 15:26:33 +0530

    Add frontend folder

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,19 @@ const { Server } = require("socket.io");
 const connectDB = require("./config/db");
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); 
 
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
 connectDB();
 
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+   origin: [
+  "http://localhost:3000",
+  process.env.FRONTEND_URL
+],
     credentials: true,
   })
 );
@@ -30,10 +34,15 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/items", require("./routes/itemRoutes"));
 app.use("/api/messages", require("./routes/messageRoutes"));
 
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      process.env.FRONTEND_URL
+    ],
     methods: ["GET", "POST"],
+    credentials: true, 
   },
 });
 
@@ -53,5 +62,8 @@ io.on("connection", (socket) => {
   });
 });
 
+
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
